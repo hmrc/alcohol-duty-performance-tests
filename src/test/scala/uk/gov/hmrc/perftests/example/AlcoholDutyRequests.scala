62120872e2152b689893cd934b80f26f2a47effc
kavita.d.kumari <kavita.d.kumari@accenture.com>
2023-12-12 01:22:30 +0000

    ADR-802 WIP

--- src/test/scala/uk/gov/hmrc/perftests/example/AlcoholDutyRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/AlcoholDutyRequests.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2023 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.perftests.example
@@ -12,8 +23,34 @@ import uk.gov.hmrc.performance.conf.ServicesConfiguration
 
 object AlcoholDutyRequests extends ServicesConfiguration {
 
-  val baseUrl: String = baseUrlFor("manage-alcohol-duty")
-  val route: String   = "/productName"
+  val baseUrl: String = baseUrlFor("alcohol-duty-returns-frontend")
+  val route: String   = "manage-alcohol-duty"
+  val authUrl: String     = baseUrlFor("auth-login-stub")
+
+  def getAuthLoginPage: HttpRequestBuilder =
+    http("Navigate to auth login stub page")
+      .get(s"$authUrl/auth-login-stub/gg-sign-in")
+      .check(status.is(200))
+      .check(regex("Authority Wizard").exists)
+      .check(regex("CredID").exists)
+
+  def loginWithAuthLoginStub(): HttpRequestBuilder = {
+
+    http("Login with user credentials")
+      .post(s"$authUrl/auth-login-stub/gg-sign-in")
+      .formParam("authorityId", "")
+      .formParam("groupIdentifier", "")
+      .formParam("credentialStrength", "Strong")
+      .formParam("confidenceLevel", "50")
+      .formParam("affinityGroup", "Individual")
+      .formParam("enrolment[0].state", "Activated")
+      .formParam("enrolment[0].name", "")
+      .formParam("enrolment[0].taxIdentifier[0].name", "")
+      .formParam("enrolment[0].taxIdentifier[0].value", "")
+      .formParam("redirectionUrl", s"$baseUrl")
+      .check(status.is(303))
+      .check(header("Location").is(s"$baseUrl": String))
+  }
 
   val navigateToProductNamePage: HttpRequestBuilder =
     http("Navigate to product Name Page")
