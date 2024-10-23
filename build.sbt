ef20d0c598a553b2dffa4d729cfab612b83aa2e6
kavita.d.kumari <kavita.d.kumari@accenture.com>
2024-10-23 09:40:48 +0100

    ADR-1394: Fixed issues after library update

--- build.sbt
+++ build.sbt
@@ -9,5 +9,5 @@ lazy val root = (project in file("."))
     // Enabling sbt-auto-build plugin provides DefaultBuildSettings with default `testOptions` from `sbt-settings` plugin.
     // These testOptions are not compatible with `sbt gatling:test`. So we have to override testOptions here.
     Test / testOptions := Seq.empty,
-    libraryDependencies ++= Dependencies.test
+    libraryDependencies ++= Dependencies.compile
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,8 @@
-import sbt._
+import sbt.{ModuleID, * }
 
 object Dependencies {
 
-  private val gatlingVersion = "3.12.0"
-
-  val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.3"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test,
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0"
   )
-
 }
